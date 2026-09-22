4f9fedb4ad2d2759a5d2938c04037771592d040a
anjali <anjalinbs@gmail.com>
2026-09-22 21:20:02 +0530

    second commit

---
+++ duplicate.py
@@ -0,0 +1,12 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array
+, and return false if every element is distinct.
+"""
+
+nums=[1,2,3,4,4]
+for i in nums:
+    if nums.count(i)>1:
+        print(True)
+        break
+else:
+    print(False)
